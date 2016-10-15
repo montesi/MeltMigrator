3f74235c3e6ff96228f5fa86d033d5688de83ce2
Hailong Bai <dragon7_@hotmail.com>
2016-10-15 13:05:08 -0400

    Update meltMain.m

--- meltMain.m
+++ meltMain.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % meltMain.m
 % Melt Migration Calculation Routine
-% Postprocess COMSOL model to calculate melt trajectories, melt flux & crustal thickness
+% Postprocess mid-ocean ridge model to calculate melt trajectories, melt flux & crustal thickness
 % Laurent Montesi, Hailong Bai after Jennifer Barry, Mark Behn, Laura Hebert
 % Department of Geology, University of Maryland, College Park, MD, 20740
-% Version 3.0, October 2015
+% Version 3.1, October 20169
 %--------------------------------------------------------------------------
 % PRE-EXISTING ------------------------------------------------------------
 %   .mph                    : 3D finite element model from COMSOL Multiphysics 4.x;
@@ -17,14 +17,12 @@
 %   SpreadingRate_Full      : Full spreading rate [cm/yr]
 %   SpreadingRate_Half      : Half-spreading rate [m/s]
 %   SpreadingDirection      : Direction of seafloor spreading (relative to x axis) [deg]
-%   T_Mantle                : Mantle temperature
 %   AdiabaticGradient       : Adiabatic gradient [degC/km]
 %   TrimDistance            : Distance to trim close to boundary [km]
-%   Switch_AsymmetryStudy   : Switch for asymmetric melting study
-%   SymmetricModel          : Reference symmetric model from COMSOL Multiphysics 4.x
 %   Switch_MeltCalibration  : Switch for melt calibration. 1: perform, 0: do not perform
 %   CrustalThickness_Reference : Reference crustal thickness [km]
 % 	CalibrationFactor       : Factor used to scale crustal thickness
+%   Switch_SaddleSelectionByMouse : Switch for saddle starting point selection method, 0: specified by user, 1: selected by mousing using ginput
 %   SaddleInitialPoint      : Starting point for saddle point search [km]
 %   SaddleSearchRadius      : Radius for saddle line search [km]
 %   SaddleFlatCutoff        : Criterion for saddle flatness
@@ -243,59 +241,19 @@
 
 %% Model Initialization
 
-% % change to working directiory
-% cd F:\Migration\MigrationArchtypeNew; % <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
 disp('>>> Melt migration calculation routine started');
-% % load comsol model
-tic;
-Model=mphload('MeltMigrationDemo_01.mph'); % <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-disp(sprintf('>>> COMSOL model loaded in %g s',toc));
-NameTag='DEMO'; % <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
+setParameters;
 
-% % key parameters
-SpreadingRate_Plate=2.0; % [cm/yr] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-SpreadingRate_etalP=2.0; % [cm/yr] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
+% % load model
+tic;
+Model=mphload(ModelName);
+fprintf('>>> Mid-ocean ridge model loaded in %g s\n',toc);
+
+% % process spreading info
 SpreadingRate_Full=SpreadingRate_Plate+SpreadingRate_etalP; % [cm/yr]
 SpreadingRate_Half=0.5*SpreadingRate_Full*(3.17e-10); % [m/s]
-SpreadingDirection=0; % 0: x-direction, 90: y-direction <<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-
-T_Mantle=1375; % mantle temperature [degC] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-AdiabaticGradient=0.6475; % adiabatic gradient [degC/km];
-
-%% Parameters
-
-TrimDistance=20; % distance to trim close to boundary [km] <<<<<<<<<<<<<<<< MODEL DEPENDENT
-
-Switch_asymmetryStudy=0; % switch for asymmetric melting study
-
-Switch_MeltCalibration=1; % switch for melt calibration. 1: perform, 0: do not perform
-CrustalThickness_Reference=6; % [km]
-% CalibrationFactor=1.7403; % <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-
-SaddleInitialPoint=[250,400;325,225;375,175]; % starting point for saddle point search <<<< MODEL DEPENDENT
-SaddleSearchRadius=10; % saddle searching radius
-SaddleFlatCutoff=0.001; % criterion for saddle flatness
-LidDepthLimit=60; % lower limit for permeability barrier [km]
-
-T_MeltSeed=190; % temperature for melt seeds generation <<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-MeltFractionCutoff=0.01;
-ExtractionWidth=[4]; % extraction width
-ExtractionDepth=[20]; % extraction depth
-ExtractionSlope=[0.1]; % critical slope
-SmoothingWidth=[60]; % smoothing intensity
-
-iFigure=1;
 
 %% Model & Segment Geometry [km]
-
-% % domain geometry <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-Geometry.ModelBoundary.x=[0,600]; 
-Geometry.ModelBoundary.y=[0,550]; 
-Geometry.ModelBoundary.z=[0,100]; 
-% % plate boundary geometry <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MODEL DEPENDENT
-Geometry.PlateBoundary.x=[200,200,300,300,400,400]; 
-Geometry.PlateBoundary.y=[550,400,400,250,150,0]; 
-Geometry.PlateBoundaryType=[1,2,1,3,1]; % 1: ridge, 2: transform, 3: oblique segment
 
 Geometry=geometryTrim(Geometry,TrimDistance); % trim geometry to avoid boundary effects
 % % plate boundary length
@@ -306,12 +264,13 @@ Geometry.PlateBoundaryLength=sqrt(Geometry.PlateBoundaryLength_x.^2+Geometry.Pla
 Geometry.PlateBoundaryAngleRad=atan(Geometry.PlateBoundaryLength_y./Geometry.PlateBoundaryLength_x)+pi*(Geometry.PlateBoundaryLength_x<0); % [rad]
 Geometry.PlateBoundaryAngleDeg=Geometry.PlateBoundaryAngleRad*180/pi; % [deg]
         
-disp(sprintf('>>> Model size: %d km x %d km x %d km',diff(Geometry.ModelBoundary.y),diff(Geometry.ModelBoundary.x),diff(Geometry.ModelBoundary.z)));
+fprintf('>>> Model size: %d km x %d km x %d km\n',diff(Geometry.ModelBoundary.y),diff(Geometry.ModelBoundary.x),diff(Geometry.ModelBoundary.z));
 
 % % plot geometry
 figure(iFigure); clf; hold on;
 plot3(Geometry.PlateBoundary.x,Geometry.PlateBoundary.y,zeros(size(Geometry.PlateBoundary.x)),'k','linewidth',2);
 box on; axis equal;
+set(gca,'boxstyle','full');
 set(gca,'zdir','reverse');
 xlim([Geometry.ModelBoundary.x]);
 ylim([Geometry.ModelBoundary.y]);
@@ -319,38 +278,18 @@ zlim([Geometry.ModelBoundary.z]);
 view(3); camproj('perspective');
 title('Model Geometry and Mesh');
 
-%% Solidus & Permeability Barrier Function
-
-% solidus=@(z) (1120+4.3092*z-5.2224e-3*z.^2-Ga*z); % solidus, Hirschmann, 2000
-% solidus=@(z) (1085.7+4.30*z-5.33e-3*z.^2); % solidus, Katz, 2003
-solidus=@(z) 1100+3*z; % solidus, Reid & Jackson, 1981
-lidTemperature=@(z) (1240+(z)*1.9-AdiabaticGradient*z); % permeability barrier temperature [degC]
-lidDepth=@(zs,Ts) fsolve(@(z)interp1(zs,Ts,z)-lidTemperature(z),0,optimset('display','off')); % permeability barrier depth [km]
-% MeltFunction=@(z,T) MeltFunctionMELTS(z,T);
-meltFunction=@(z,T) meltFunctionRJ1981(z,T);
-
 %% Resolution
 
-Res.x=5; % x resolution [km]
-Res.y=5; % y resolution [km]
 Res.nx=ceil(diff(Geometry.ModelBoundary.x)/Res.x)+1; % sampling size, x direction
 Res.ny=ceil(diff(Geometry.ModelBoundary.y)/Res.y)+1; % sampling size, y direction
-Res.nz=25; % % sampling size, z direction
-Res.zFactor=1.2; % z nonlinear factor
-Res.MeltCalibration=10; % melt calibration resolution
-Res.Saddle=1; % saddle line resolution
-Res.MeltSeed=5; % melt line seed resolution
-Res.MeltTrajectory=5; % melt line resolution
-Res.MeltSwath=4; % melt swath resolution
 Res.nMeltSwath=ceil(100/Res.MeltSwath); % melt swath sampling size
-Res.Axis=1; % plate boundaries resolution for crustal thickness calculation 
 
 Vector.x=linspace(Geometry.ModelBoundary.x(1),Geometry.ModelBoundary.x(2),Res.nx);
 Vector.y=linspace(Geometry.ModelBoundary.y(1),Geometry.ModelBoundary.y(2),Res.ny);
 Vector.z=linspace(0,max(Geometry.ModelBoundary.z)^(1/Res.zFactor),Res.nz).^Res.zFactor; % nonlinear vertical sampling vector
 [Grid.x,Grid.y]=meshgrid(Vector.x,Vector.y);
 [Box.x,Box.y,Box.z]=meshgrid(Vector.x,Vector.y,Vector.z);
-disp(sprintf('>>> Sampling size: %d x %d x %d = %d points',size(Box.x),numel(Box.x)));
+fprintf('>>> Sampling size: %d x %d x %d = %d points\n',size(Box.x),numel(Box.x));
 
 % % plot mesh
 figure(iFigure);
@@ -359,93 +298,85 @@ mesh(Grid.x,Grid.y,zeros(size(Grid.x)));
 mesh(GridTemp.x,zeros(size(GridTemp.x))+TrimDistance,GridTemp.z);
 [GridTemp.y,GridTemp.z]=meshgrid(Vector.y,Vector.z);
 mesh(zeros(size(GridTemp.y))+TrimDistance,GridTemp.y,GridTemp.z);
-for iSaddlePoint=1:size(SaddleInitialPoint,1);
-    scatter(SaddleInitialPoint(iSaddlePoint,1),SaddleInitialPoint(iSaddlePoint,2),250,'fill','markeredgecolor','k','markerfacecolor','r','linewidth',1.5);
+if ~Switch_SaddleSelectionByMouse
+    for iSaddlePoint=1:size(SaddleInitialPoint,1)
+        scatter(SaddleInitialPoint(iSaddlePoint,1),SaddleInitialPoint(iSaddlePoint,2),250,'fill','markeredgecolor','k','markerfacecolor','r','linewidth',1.5);
+    end
 end
 clear GridTemp;
 
 %% Lid Sampling
 
 tic;
-disp(sprintf('>>> Sampling permeability barrier'));
-[Lid,Box]=lidSample(Model,Box,Grid,Vector,Geometry,T_Mantle,solidus,lidDepth); % sample lid
+fprintf('>>> Sampling permeability barrier\n');
+lidTemperature=@(z) (1240+(z)*1.9-AdiabaticGradient*z); % permeability barrier temperature [degC]
+lidDepth=@(zs,Ts) fsolve(@(z)interp1(zs,Ts,z)-lidTemperature(z),0,optimset('display','off')); % permeability barrier depth [km]
+[Lid,Box]=lidSample(Model,Box,Grid,Vector,Geometry,solidus,lidDepth); % sample lid
 save(sprintf('%s_Lid.mat',NameTag),'Lid');
 save(sprintf('%s_Box.mat',NameTag),'Box');
 plotLidInfo(Geometry,Grid,Lid,iFigure+1); % plot lid information
 print(iFigure+1,'-dpdf','-r300',sprintf('%s_F%d_Lid.pdf',NameTag,iFigure+1));
-disp(sprintf('>>> Permeability barrier sampled in %g s',toc));
+fprintf('>>> Permeability barrier sampled in %g s\n',toc);
 
 %% Melting Calibration
 
-if Switch_MeltCalibration;
+if Switch_MeltCalibration
     tic;
     disp('>>> Calibrating melting function');
     CalibrationFactor=meltCalibration(Model,Geometry,Grid,Vector,Res,Lid,meltFunction,lidTemperature,...
         SpreadingDirection,SpreadingRate_Half,MeltFractionCutoff(1),CrustalThickness_Reference,iFigure);
     meltFunction=@(z,T) meltFunction(z,T)./CalibrationFactor; % calibrate melting function to generate reasonable crustal thickness 
-    disp(sprintf('>>> Obtained %g km of crust at reference section with CalibrationFactor=%g, in %g s',CrustalThickness_Reference,CalibrationFactor,toc));
+    fprintf('>>> Obtained %g km of crust at reference section with CalibrationFactor=%g, in %g s\n',CrustalThickness_Reference,CalibrationFactor,toc);
 end
-
-%% Asymmetry Study
-
-if Switch_asymmetryStudy~=0;
-    tic;
-    disp(sprintf('>>> Comparing current model with a reference symmetric model'));
-    SymmetricModel=mphload('NEPR_etae_ke_Ref_15_20.mph'); % <<<<<<<<<<<<<<< MODEL DEPENDENT
-    Box=asymmetryStudy(SymmetricModel,Geometry,Vector,Box,meltFunction,40,iFigure+2);
-    save(sprintf('%s_Box.mat',NameTag),'Box');
-    print(iFigure+2,'-dpdf','-r300',sprintf('%s_F%d_AsymmetricMelting.pdf',NameTag,iFigure+2));
-    print(iFigure+3,'-dpdf','-r300',sprintf('%s_F%d_AsymmetricVelocity.pdf',NameTag,iFigure+3));
-    disp(sprintf('>>> Asymmetry comparison completed in %gs',toc));
-else
-    iFigure=-1;
-end
-clear SymmetricModel;
 
 %% Saddle Calculation
 
 tic;
-disp(sprintf('>>> Performing saddle calculation'));
+fprintf('>>> Performing saddle calculation\n');
+if Switch_SaddleSelectionByMouse
+    figure(iFigure+1);
+    SaddleInitialPoint=ginput; % select starting points for saddle point search by mouse click on lid maps
+end
 Saddle=saddlePreparation(Geometry,Grid,Res,Lid,SaddleInitialPoint,SaddleSearchRadius,SaddleFlatCutoff,iFigure+4);
 save(sprintf('%s_Saddle.mat',NameTag),'Saddle');
 print(iFigure+4,'-dpdf','-r300',sprintf('%s_F%d_Saddle.pdf',NameTag,iFigure+4));
-disp(sprintf('>>> Saddle completed in %gs',toc));
+fprintf('>>> Saddle completed in %gs\n',toc);
 
 %% Melt Line Seeds Generation
 
 tic;
-disp(sprintf('>>> Generating melt seeds')); 
+fprintf('>>> Generating melt seeds\n'); 
 Seed=meltTrajectorySeed(Geometry,Grid,Res,Lid,T_MeltSeed,SpreadingDirection,iFigure+5);
 save(sprintf('%s_Seed.mat',NameTag),'Seed');
 print(iFigure+5,'-dpdf','-r300',sprintf('%s_F%d_Seed.pdf',NameTag,iFigure+5));
-disp(sprintf('>>> Melt seeds generated in %gs',toc));
+fprintf('>>> Melt seeds generated in %gs\n',toc);
 
 %% Melt Line Calculation
 
 tic;
-disp(sprintf('>>> Calculating melt trajectories')); 
+fprintf('>>> Calculating melt trajectories\n'); 
 Shoulder=meltTrajectory(Geometry,Grid,Res,Lid,Saddle,Seed,iFigure+6);
 save(sprintf('%s_Shoulder.mat',NameTag),'Shoulder');
 print(iFigure+6,'-dpdf','-r300',sprintf('%s_F%d_Shoulder.pdf',NameTag,iFigure+6));
-disp(sprintf('>>> Melt trajectories calculated in %gs',toc));
+fprintf('>>> Melt trajectories calculated in %gs\n',toc);
 
 %% Melt Swath Generation
 
 tic;
-disp(sprintf('>>> Creating melt swaths and polygons')); 
+fprintf('>>> Creating melt swaths and polygons\n'); 
 Shoulder=meltSwath(Model,Geometry,Grid,Res,Lid,Shoulder,LidDepthLimit,iFigure+7);
 save(sprintf('%s_Shoulder.mat',NameTag),'Shoulder');
 print(iFigure+7,'-dpdf','-r300',sprintf('%s_F%d_Swath.pdf',NameTag,iFigure+7));
-disp(sprintf('>>> Melt swaths and polygons created in %gs',toc));
+fprintf('>>> Melt swaths and polygons created in %gs\n',toc);
 
 %% Crustal Thickness Calculation
 
 tic;
-disp(sprintf('>>> Calculating melt flux and crustal thickness')); 
+fprintf('>>> Calculating melt flux and crustal thickness\n'); 
 [Parameter,Result,ResultSummary]=crustCalculation(NameTag,Geometry,Res,Shoulder,meltFunction,SpreadingDirection,SpreadingRate_Half,...
     TrimDistance,ExtractionWidth,ExtractionDepth,ExtractionSlope,MeltFractionCutoff,SmoothingWidth,iFigure+8);
 save(sprintf('%s_Parameter.mat',NameTag),'Parameter');
 save(sprintf('%s_Result.mat',NameTag),'Result');
 save(sprintf('%s_ResultSummary.mat',NameTag),'ResultSummary');
-disp(sprintf('>>> Melt flux and crustal thickness calculated in %gs',toc));tic;
+fprintf('>>> Melt flux and crustal thickness calculated in %gs\n',toc);
 disp('>>> Melt migration calculation routine completed');
